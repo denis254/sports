e4e7fab0c6ae7a5f204fde643bea0230fa5823f0
denis kiprono <deniskiprono4@gmail.com>
2018-07-30 09:00:15 +0300

    First

--- sports/settings.py
+++ sports/settings.py
@@ -118,12 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
-STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Games/')
 
 SITE_ID = 1
 
